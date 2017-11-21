7c3a2f9bf3653535d1af8119d4f056eedcab68cc
George <georgetw88@outlook.com>
2017-11-21 22:07:00 +1300

    firebase integ completed

---
+++ SlopesSocial/Controller/LoginPageVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlopesSocial
+//
+//  Created by George Woolley on 21/11/2017.
+//  Copyright © 2017 George Woolley. All rights reserved.
+//
+
+import UIKit
+
+class LoginPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
